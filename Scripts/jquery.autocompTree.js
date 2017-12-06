1f2bbd8ad52096c5ec4836b42bd47e5518102070
rkjdid <romainknezevic@gmail.com>
2017-12-06 17:01:35 +0100

    reformat jquery.autocompTree.js (no change)

--- Scripts/jquery.autocompTree.js
+++ Scripts/jquery.autocompTree.js
@@ -6,70 +6,71 @@
 *       - JQuery UI (min 1.8.*)
 *       - Fancytree (importé la bibliothèque jquery.fancytree-all ainsi que son css)
 */
-(function ($) {
-    $.fn.autocompTree = function (methodOrOptions) {
+(function($) {
+    $.fn.autocompTree = function(methodOrOptions) {
         //On définit nos paramètres par défaut
         var _self = $(this);
         var defauts =
-        {
-            //Les webservices doivent renvoyer une chaine au format JSON contenant au moins ces information:
-            /*
-            *   treeElt {
-            *       title : string
-            *       key : int
-            *       lazy : false //L'autocomplete n'étant pas du tout efficace en lazy loading
-            *       //Enfin toues les information qui seront utile a son utilisation
-            *       //Notamment la valeur a afficher (et ou stocké si isDisplayDifferent = false)
-            *       //Eventuellement la valeur à stocké (si isDisplayDifferent = true)
-            *   }
-            */
-            //URL des webservices
-            wsUrl: 'http://' + window.location.hostname + '/Thesaurus/App_WebServices/wsTTopic.asmx',
-            //Webservices pour un affichage en arborescence
-            webservices: 'initTreeByIdWLanguageWithoutDeprecated',
-            //si l'affichage est différent de la valeur renvoyée
-            display: {
-                isDisplayDifferent: true,
-                //Stocke la valeur dans un input hidden d'id = _self.attr("id") + suffixeId
-                suffixeId: '_value',
-                //Nom des paramètres a récupéré dans les noeuds de l'arbre
-                //Si isDisplayDifferent = false -> la valeur sera displayValueName
-                //Affichage
-                displayValueName: 'fullpathTranslated',
-                //Valeur cachée
-                storedValueName: 'fullpath'
-            },
-            //Si l'arbre utilise un langage différent
-            language: {
-                hasLanguage: false,
-                //Préféré un majuscule en première lettre
-                lng: "En"
-            },
-            //Si l'arbre possède déjà une valeur
-            inputValue: '',
-            //idServant à l'initialisation de l'arbre
-            //TODO service de paramètrage dynamique ou tous les paramètres ainsi que leur valeur sont généré a la volée
-            startId: '',
-            //Fonction appelé a chaque fin d'éxécution
-            callback: '',
-            //Fonction s'éxecutant après un clique
-            onItemClick: '',
-            //Fonction s'éxécutant après le focus sur l'input
-            onInputFocus: '',
-            //Fonction s'éxécutant après la perte de focus de l'input et de l'arbre
-            onInputBlur: '',
-            //Fonction s'éxécutant après l'initialisation de l'objet autocompTree
-            onInputInitialize: '',
-            //Enregistrement de l'item en elle même
-            thisItem: $(this)
-        }
+            {
+                //Les webservices doivent renvoyer une chaine au format JSON contenant au moins ces information:
+                /*
+                *   treeElt {
+                *       title : string
+                *       key : int
+                *       lazy : false //L'autocomplete n'étant pas du tout efficace en lazy loading
+                *       //Enfin toues les information qui seront utile a son utilisation
+                *       //Notamment la valeur a afficher (et ou stocké si isDisplayDifferent = false)
+                *       //Eventuellement la valeur à stocké (si isDisplayDifferent = true)
+                *   }
+                */
+                //URL des webservices
+                wsUrl: 'http://' + window.location.hostname + '/Thesaurus/App_WebServices/wsTTopic.asmx',
+                //Webservices pour un affichage en arborescence
+                webservices: 'initTreeByIdWLanguageWithoutDeprecated',
+                //si l'affichage est différent de la valeur renvoyée
+                display: {
+                    isDisplayDifferent: true,
+                    //Stocke la valeur dans un input hidden d'id = _self.attr("id") + suffixeId
+                    suffixeId: '_value',
+                    //Nom des paramètres a récupéré dans les noeuds de l'arbre
+                    //Si isDisplayDifferent = false -> la valeur sera displayValueName
+                    //Affichage
+                    displayValueName: 'fullpathTranslated',
+                    //Valeur cachée
+                    storedValueName: 'fullpath'
+                },
+                //Si l'arbre utilise un langage différent
+                language: {
+                    hasLanguage: false,
+                    //Préféré un majuscule en première lettre
+                    lng: "En"
+                },
+                //Si l'arbre possède déjà une valeur
+                inputValue: '',
+                //idServant à l'initialisation de l'arbre
+                //TODO service de paramètrage dynamique ou tous les paramètres ainsi que leur valeur sont généré a la volée
+                startId: '',
+                //Fonction appelé a chaque fin d'éxécution
+                callback: '',
+                //Fonction s'éxecutant après un clique
+                onItemClick: '',
+                //Fonction s'éxécutant après le focus sur l'input
+                onInputFocus: '',
+                //Fonction s'éxécutant après la perte de focus de l'input et de l'arbre
+                onInputBlur: '',
+                //Fonction s'éxécutant après l'initialisation de l'objet autocompTree
+                onInputInitialize: '',
+                //Enregistrement de l'item en elle même
+                thisItem: $(this)
+            }
 
         var methods = {
-            init: function (parametres) {                
+            init: function(parametres) {
                 //Fusion des paramètres envoyer avec les params par defaut
                 if (parametres) {
                     var parametres = $.extend(defauts, parametres);
-                };
+                }
+                ;
                 //Information à envoyer 
                 var dataToSend = '';
                 if (parametres.language.hasLanguage) {
@@ -78,7 +79,7 @@
                     dataToSend = "{'id':'" + parametres.startId + "'}"
                 }
 
-                _self.each(function () {
+                _self.each(function() {
                     var $me = $(this);
                     //On encapsule l'input ainsi que tous les éléments dans un div afin de les contrôlés
                     $me.wrapAll('<div id="divAutoComp_' + $me.attr("id") + '">');
@@ -116,7 +117,7 @@
                             data: dataToSend
                         },
                         //Permet si l'arbre et en mode filter d'afficher les enfants des termes filtrés -> submatch
-                        renderNode: function (event, data) {
+                        renderNode: function(event, data) {
                             var node = data.node;
                             if (data.tree.options.hideExpand.isHide) {
                                 data.tree.options.hideExpand.nbExpand--;
@@ -130,7 +131,7 @@
                             }
                         },
                         //Servant ici a afficher les termes enfants des termes filtré
-                        click: function (event, data) {
+                        click: function(event, data) {
                             var node = data.node,
                                 tt = $.ui.fancytree.getEventTargetType(event.originalEvent);
                             //Bubbles permet de déterminer si l'evt vient d'un click souris oud'un faux clique setExpand
@@ -144,7 +145,7 @@
                             }
                         },
                         //evenement d'activation de l'arbre (au clique)
-                        activate: function (event, data) {
+                        activate: function(event, data) {
                             var tree = $("#treeView" + $me.attr("id")).fancytree('getTree');
                             if (parametres.display.isDisplayDifferent) {
                                 $me.val(data.node.data[parametres.display.displayValueName]);
@@ -169,15 +170,18 @@
                         }
                     });
                     //Permet l'affichage du treeview au focus sur l'input
-                    $me.focus(function () {
-                        $("div[id^=treeView]").each(function () {
+                    $me.focus(function() {
+                        $("div[id^=treeView]").each(function() {
                             $(this).css('display', 'none');
                         });
                         var treeContainer = $("#treeView" + $me.attr("id"));
                         treeContainer.css('display', 'block').css('width', $me.outerWidth() - 2).css('border', 'solid 1px').css('position', 'absolute').css('z-index', '100');
-                        treeContainer.offset({ left: $(this).offset().left, top: $(this).position().top + $(this).outerHeight() });
+                        treeContainer.offset({
+                            left: $(this).offset().left,
+                            top: $(this).position().top + $(this).outerHeight()
+                        });
                         //Fonction qui permet d'effectuer un "blur" sur l'ensemble des éléments (input et arbre)
-                        $(document).delegate("body", "click", function (event) {                            
+                        $(document).delegate("body", "click", function(event) {
                             if (!$(event.target).is("#" + $me.attr("id") + ",span[class^=fancytree], div[id^=treeView], ul")) {
                                 var treeContainer = $("#treeView" + $me.attr("id"));
                                 treeContainer.css('display', 'none');
@@ -200,13 +204,13 @@
                         }
                     });
                     //Fonction de recherche et de filtration
-                    $me.keyup(function (e) {
+                    $me.keyup(function(e) {
                         var treeHtml = $("#treeView" + $me.attr("id"));
                         var fancytree = treeHtml.fancytree("getTree");
                         //Si le nombrte d'élément est < a 100 on oblige l'utilisation d'au moins trois caractère pour des raisons de performances
                         if (fancytree.count() < 100 || $me.val().length >= 3) {
                             treeHtml.find('ul.fancytree-container li').css("padding", "1px 0 0 0");
-                            treeHtml.fancytree("getRootNode").visit(function (node) {
+                            treeHtml.fancytree("getRootNode").visit(function(node) {
                                 if (node.span) {
                                     var className = node.span.className;
                                     if (className.indexOf('fancytree-hide') != -1) {
@@ -234,14 +238,14 @@
                         }
                         if ($me.val().length == 0) {
                             fancytree.clearFilter();
-                            treeHtml.fancytree("getRootNode").visit(function (node) {
+                            treeHtml.fancytree("getRootNode").visit(function(node) {
                                 node.setExpanded(false);
                             });
                         }
                     });
 
                     if (parametres.display.isDisplayDifferent) {
-                        $me.change(function () {
+                        $me.change(function() {
                             $("#" + $me.attr('id') + parametres.display.suffixeId).val($me.val());
                         });
                     }
@@ -256,7 +260,7 @@
                 });
                 return _self;
             },
-            reload: function (source) {
+            reload: function(source) {
                 var tree = $("#treeView" + _self.attr('id')).fancytree('getTree');
                 if (source) {
                     //Possibilité de modifier la source de l'arbre
@@ -265,7 +269,7 @@
                 tree.reload();
                 return tree;
             },
-            getTree: function () {
+            getTree: function() {
                 return $("#treeView" + _self.attr('id')).fancytree('getTree');
             }
         }
